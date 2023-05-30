a459115e7f922c257982a1959604297d4d04593e
badbois <jordan.roberty@outlook.fr>
2023-05-30 23:27:32 +0200

    modify the target of the github's link

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 const  Footer = () => {
     return ( 
         <nav className="footer">
-            <p>Site web Réalisé par <a href="https://github.com/badbois">Jordan ROBERTY</a> </p>
+            <p>Site web Réalisé par <a href="https://github.com/badbois" target='_blank'>Jordan ROBERTY</a> </p>
         </nav>
     );
 }
