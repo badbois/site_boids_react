467b29247b53793acf35a492bf7609115cba5a0c
badbois <jordan.roberty@outlook.fr>
2023-05-30 20:47:35 +0200

    Ouvre les liens dans un nouvel onglet

--- src/p5example/BoidsManager.js
+++ src/p5example/BoidsManager.js
@@ -33,7 +33,7 @@ function BoidsManager() {
         <div className="boidsManager"> 
             <h1>Exemple de d'implémentation</h1>
             <Sketch setup={setup} draw={draw} />;
-            <p>Dans cette implémentation, nous pouvons voir des boids en train d'évoluer naturellement. Néanmoins, beaucoup d'améliorations peuvent être misent en place comme par exemple le fait d'avoir des rotations plus fluides ou encore d'ajouter des comportements comme l'esquive d'obstacle ou bien avoir différents groupes de boids. Merci à Daniel Shiffman pour sa chaine <a href='https://www.youtube.com/@TheCodingTrain'> TheCodingTrain</a> grâce à laquelle j'ai appris beaucoup de chose. Vous pouvez retrouver un code de boids fonctionnels et interractifs <a href='https://editor.p5js.org/badbois/sketches/ri2a72pON'> ici.</a> </p>
+            <p>Dans cette implémentation, nous pouvons voir des boids en train d'évoluer naturellement. Néanmoins, beaucoup d'améliorations peuvent être misent en place comme par exemple le fait d'avoir des rotations plus fluides ou encore d'ajouter des comportements comme l'esquive d'obstacle ou bien avoir différents groupes de boids. Merci à Daniel Shiffman pour sa chaine <a href='https://www.youtube.com/@TheCodingTrain' target='_blank'> TheCodingTrain</a> grâce à laquelle j'ai appris beaucoup de chose. Vous pouvez retrouver un code de boids fonctionnels et interractifs <a href='https://editor.p5js.org/badbois/sketches/ri2a72pON' target='_blank'> ici.</a> </p>
         </div>
     )
 }
