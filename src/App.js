4cb330443b6f2876d64062a5d364121fcc26422f
badbois <jordan.roberty@outlook.fr>
2023-05-30 16:22:53 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Footer from './Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BoidsManager from './p5example/BoidsManager';
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
           {<Routes>
             <Route exact path='/' element={<Home/>} />
           </Routes> }
+          <BoidsManager/>
+          <Footer/>
         </div>
       </div>
     </Router>
