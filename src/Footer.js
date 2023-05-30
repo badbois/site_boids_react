7437de47c7a11fff7cc3c36dff1958beb19b8576
badbois <jordan.roberty@outlook.fr>
2023-05-30 16:23:18 +0200

    add footer

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const  Footer = () => {
+    return ( 
+        <nav className="footer">
+            <p>Site web Réalisé par Jordan ROBERTY</p>
+        </nav>
+    );
+}
+ 
+export default Footer ;
